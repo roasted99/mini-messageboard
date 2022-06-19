e283bf7d0eef939ed54d1b490ad55f6de7bc6e12
Kham Laung <shwegyimontkyut@gmail.com>
2022-06-19 13:45:39 +0630

    add post method for new message

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,16 @@ const messages = [
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
+  next();
 });
 
+router.get('/new', (req, res) => {
+  res.render('form')
+});
 
+router.post('/new', (req, res)=> {
+  messages.push({text: req.body.messageText, user: req.body.messageUser, added: new Date()});
+  res.redirect('/')
+})
 
 module.exports = router;
